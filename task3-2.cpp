8edd15ea6c76fb0a63d719ba39caf84920a9fcbf
zaptot <zaptot@gmail.com>
2016-10-30 23:21:19 +0300

    Add files via upload

---
+++ task3-2.cpp
@@ -0,0 +1,61 @@
+//В массиве размера N, заполненного случ.числами от 0 до 10, подсчитать количество элементов, встречающихся более одного раза.
+#include <iostream>
+#include <ctime>
+#include <clocale>
+void printA(int A[], int N);
+void initA(int A[], int N);
+int checkA(int A[], int N);
+using namespace std;
+int main()
+{
+	const int N = 10;
+	int A[N];
+	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+	initA(A, N);
+	printA(A, N);
+	cout << "\nколичество повторяющихся элементов="<<checkA(A,N)<<'\n';
+	system("PAUSE");
+}
+void printA(int A[], int N)
+{
+	int i = 0;
+	while (i<N)
+	{
+		cout << A[i] << "  ";
+		i++;
+	}
+}
+void initA(int A[], int N)
+{
+	int i = 0;
+
+	while (i<N)
+	{
+		A[i] = rand() % 10;
+		i++;
+	}
+}
+int checkA(int A[], int N)
+{
+    int countOfDiffElements=0,i=0,j;
+	while (i < N)
+	{
+		j = i + 1;
+		while (j<N)
+		{
+			if (A[i] >= 0 && A[i] == A[j])
+			{
+				countOfDiffElements++;
+				while (j < N)
+				{
+					if (A[i] == A[j]) A[j] = -1;
+					j++;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (countOfDiffElements);
+}
