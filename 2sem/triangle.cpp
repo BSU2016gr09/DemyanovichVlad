773dabbb24ba46bbeb327c578abb2589eae80737
barvenov <barvenovsa@gmail.com>
2017-04-20 22:04:32 +0200

    недоделки большие...

--- 2sem/triangle.cpp
+++ 2sem/triangle.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 class triangle {
 public:
-    int x[3];
-    int y[3];
-    triangle () {
+    int x[3];//Плохо, что это public
+    int y[3];//Плохо, что это public
+    triangle () {//А почему нет конструктора с параметрами? Как мне без него в main создать массив, например, треугольников?
         s=0;
         for (int i=0; i<3; i++) {
             side[i]=0;
@@ -23,7 +23,7 @@ public:
         }
         return *this;
     }
-        triangle& operator +(triangle other) {
+        triangle& operator +(triangle other) {//вот интересно мне как математику - что такое сумма треугольников в вашей интерпретации????
         cout << "Operator of addition is working\n";
         this->s += other.s;
         this->p += other.p;
@@ -32,7 +32,7 @@ public:
         }
         return *this;
     }
-        triangle& operator -(triangle other) {
+        triangle& operator -(triangle other) {//вот интересно мне как математику - что такое разность треугольников в вашей интерпретации????
         cout << "Operator of subtraction is working\n";
         this->s -= other.s;
         this->p -= other.p;
@@ -47,7 +47,7 @@ public:
     void calcSides() {//глагол ВЫЧИСЛИТЬ пишется как cAlculate
         int j = 0;
         for (int i = 0; i < 3; i++) {
-            if (i < 2) j = i+1; else j = 0;
+            if (i < 2) j = i+1; else j = 0; // а проверка. что треугольник существует????
             side[i] = sqrt((this->x[j] - this->x[i])*(this->x[j] - this->x[i]) + (this->y[j] - this->y[i])*(this->y[j] - this->y[i]));
         }
     }
@@ -61,7 +61,7 @@ public:
     void area() {//Площадь называется AREA
         int halfP = p / 2;
         
-        s = sqrt(abs(halfP*(halfP-side[0])*(halfP-side[1])*(halfP-side[2])));
+        s = sqrt(abs(halfP*(halfP-side[0])*(halfP-side[1])*(halfP-side[2])));// а проверка. что треугольник существует????
     }
     void print() {//Зачем в названии метода буква Т?????
         cout << "first: "<< side[0] << "  second:  "<< side[1] << "  third:  "<< side[2]<<" perimeter: "<<p<<" area: "<< s;
